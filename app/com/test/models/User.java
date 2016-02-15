6a311322d66994be3429fe6385f4103a86fcf1c7
Narendra <narendrar@egnaroinc.com>
2016-02-15 15:56:24 +0530

    Added User model

---
+++ app/com/test/models/User.java
@@ -0,0 +1,32 @@
+package com.test.models;
+
+public class User {
+
+	private Long id;
+	private String userName;
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
